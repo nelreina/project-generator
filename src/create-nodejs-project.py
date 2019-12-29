52a0da5e91bfd6a4faa115950a12f940550ea7d3
Nelson Reina (@nelreina) <nelreina@gmail.com>
2019-12-28 22:10:58 -0400

    cnode: rewrite to use create_from_template

--- src/create-nodejs-project.py
+++ src/create-nodejs-project.py
@@ -1,37 +1,19 @@
 import os
 from pathlib import Path
 
-from lib import create_file, add_git, create_project
+from lib import create_from_template, add_git, create_project
 
 
 if __name__ == "__main__":
     DIR_PROJECT_FOLDER, args = create_project("nodejs")
     project = args.project
-    pjson = '''{{
-  "name": "{}",
-  "version": "1.0.0",
-  "main": "src/index.js",
-  "scripts": {{
-    "dev": "nodemon --watch src src/index --dev=true",
-    "start": "node src/index"
-  }},
-  "license": "MIT"
-}}'''.format(project)
 
     print("Create package.json file")
     file_name = Path.joinpath(DIR_PROJECT_FOLDER, "package.json")
-    create_file(file_name, pjson)
+    create_from_template("cnode_package_json", file_name, {"project": project})
 
     file_name = Path.joinpath(DIR_PROJECT_FOLDER, "src/index.js")
-    create_file(file_name, f"""require('dotenv').config();
-const log4js = require('@nelreina/node-log4js');
-    
-const argv = require('minimist')(process.argv.slice(2));
-const logger = log4js('{project}');
-const DEBUG = process.env['DEBUG'];
-
-logger.info(`Start Project {project}: ${{JSON.stringify({{DEBUG, argv}})}}  `)
-    """)
+    create_from_template("cnode_index_js", file_name, {"project": project})
 
     pcks = 'moment string lodash axios dotenv minimist @nelreina/node-log4js '
     if args.packages:
@@ -43,3 +25,5 @@ logger.info(`Start Project {project}: ${{JSON.stringify({{DEBUG, argv}})}}  `)
     add_git(project)
 
     os.system("code .")
+    os.chdir(DIR_PROJECT_FOLDER)
+    os.system("yarn dev")
