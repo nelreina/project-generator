b229fcb27e189ae93fd262735176e6c82fd9eb80
Nelson Reina (@nelreina) <nelreina@gmail.com>
2020-01-11 20:52:14 -0400

    make create python app callable from other modules

--- src/create-python-project.py
+++ src/create-python-project.py
@@ -4,19 +4,25 @@ from pathlib import Path
 from lib import create_from_template, add_git, create_project
 
 
-if __name__ == "__main__":
+def run(pip=True, open_vs_code=True):
     DIR_PROJECT_FOLDER, args = create_project()
     project = args.project
 
     file_name = Path.joinpath(DIR_PROJECT_FOLDER, "src/app.py")
     create_from_template("cpy_app_py", file_name, {"project": project})
 
-    print("Init pip environment")
-    os.system("pipenv install requests argparse python-dotenv")
-    os.system("pipenv install autopep8 pyinstaller --dev")
-    if args.packages:
-        os.system('pipenv install ' + args.packages)
+    if pip:
+        print("Init pip environment")
+        os.system("pipenv install autopep8 pyinstaller --dev")
+        os.system("pipenv install requests python-dotenv")
+        if args.packages:
+            os.system('pipenv install ' + args.packages)
 
     add_git(project)
-    os.chdir(DIR_PROJECT_FOLDER)
-    os.system("code . src/app.py")
+    if open_vs_code:
+        os.chdir(DIR_PROJECT_FOLDER)
+        os.system("code . src/app.py")
+
+
+if __name__ == "__main__":
+    run()
